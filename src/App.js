1b49283bdd8d79f35797af62c69372ff0c5a55dd
charankrishna <charan.nithinj@gmail.com>
2018-09-10 00:03:31 +0530

    DummyWebsite

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './components/navbar';
+import Home from './components/home';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div className="App">   
+          <Nav/>
+          <Switch>
+          <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={About} />
+            <Route exact path='/portfolio' component={Portfolio} />
+          </Switch>
       </div>
+      </Router>
     );
   }
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+var NavLink = require('react-router-dom').NavLink;
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div className="container">
+        <NavLink className="navbar-brand" to='/'>Racks</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="oi oi-menu"></span> Menu
+        </button>
+
+        <div className="collapse navbar-collapse" id="ftco-nav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active"><NavLink className="nav-link" to='/'>Home</NavLink></li>
+            <li className="nav-item"><NavLink href="about.html" className="nav-link" to='/About'>About</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to='/Solution'>Solution</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to='/portfolio'>Portfolio</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to='/Blog'>Blog</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to='/Contact'>Contact</NavLink></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
